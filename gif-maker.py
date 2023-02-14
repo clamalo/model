a89e305fd6cb0b79c27d21cef01fc7fb14016b4f
clamalo <89725263+clamalo@users.noreply.github.com>
2023-02-14 14:45:28 -0800

    Add files via upload

---
+++ gif-maker.py
@@ -0,0 +1,20 @@
+with open('model_config.txt', 'r') as myfile:
+    text=myfile.read()
+    text = text.split('\n')
+    for arg in range(len(text)):
+        config = text[arg].split('=')[1]
+        config = config.replace(' ','')
+        text[arg] = config
+    directory = text[0]
+
+import imageio
+
+step = int(input('Step: '))
+start_frame = int(input('Start frame: '))
+end_frame = int(input('End frame: '))
+parameter = input('Parameter: ')
+
+images = []
+for frame_number in range(start_frame,end_frame+1,step):
+    images.append(imageio.imread('figures/'+parameter+'/'+parameter+'_'+str(frame_number)+'.png'))
+imageio.mimsave(directory+'figures/custom.gif', images, duration=0.2)
